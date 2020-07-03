82f06653d7dee05ff40ad8b9e98c60f0adf3ebed
Marcelo Gomes Ferreira <mgomes@marlin.net>
2020-07-02 22:05:05 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>
+        Economídias
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="stylesheet" type="text/css" href="dist/css/style-gpu.css?v=20190909v1">
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="dist/lib/slick/slick.min.js"></script>
+    <script type="text/javascript" src="dist/lib/jquery.mask.js"></script>
+    <link rel="stylesheet" type="text/css" href="dist/lib/slick/slick.css" />
+    <script type="text/javascript" src="dist/js/app.js?v=20190830v1"></script>
+    <!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> -->
+    <link href="dist/lib/fontawesome-free-5.9.0-web/css/all.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <meta name='description' content='' />
+    <meta name='keywords' content='' />
+    <meta property="og:image" content="">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:type" content="article">
+    <meta property="og:locale" content="pt-br">
+    <meta property="og:site_name" content="">
+</head>
+
+<body class="BodyClassic">
+    <div class="BodyClassic_Home">
+        <div class="BodyClassic_Home_Content">
+            <div class="Home_Content_Form">
+                <div class="Content_Form_Titulo">
+                    Faça seu login no <span>Sistema Economídias</span>
+                </div>
+                <div class="Content_Form_Container Formgroup">
+                    <div class="Formgroup_Item">
+                        <div class="Formgroup_Item_Label">
+                            <label for="user">Usuário:</label>
+                        </div>
+                        <div class="Formgroup_Item_Input">
+                            <input placeholder="joao" for="user">
+                        </div>
+                    </div>
+                    <div class="Formgroup_Item">
+                        <div class="Formgroup_Item_Label">
+                            <label for="user">Senha:</label>
+                        </div>
+                        <div class="Formgroup_Item_Input">
+                            <input placeholder="****" for="user">
+                        </div>
+                    </div>
+                    <div class="Formgroup_Item">
+                        <div class="Formgroup_Item_Button">
+                            <button>
+                                Entrar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
